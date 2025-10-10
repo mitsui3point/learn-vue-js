856b484c0d13cf17b3af858729a80f16043b81f3
mitsui3point <rladbsgh0331@gmail.com>
2025-10-11 02:29:32 +0900

    fix(vue-form): add axios 통신

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <form action="/" v-on:submit="submitForm">
+  <form action="/" v-on:submit.prevent="submitForm">
     <div>
       <label for="username">id:</label>
       <input 
@@ -19,7 +19,7 @@
 </template>
 
 <script>
-
+import axios from 'axios';
 export default {
   data: function() {
     return {
@@ -28,9 +28,19 @@ export default {
     }
   },
   methods: {
-    submitForm: function(event) {
-      event.preventDefault();
-      console.log(this.username, this.password);
+    submitForm: function() {
+      const url = 'https://jsonplaceholder.typicode.com/users';
+      const data = {
+        username: this.username,
+        password: this.password
+      };
+      axios.post(url, data)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
     }
   }
 }
